421d7031aa95dfdccb17ec69b954c29b5a52bc2c
Ronan Girault <girault.ronan@gmail.com>
2020-07-11 15:35:29 +0200

    init pet clinic project

---
+++ src/test/java/spring/guru/udemy/sfgpetclinic/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.guru.udemy.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
